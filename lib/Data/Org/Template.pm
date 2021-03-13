56cb0b01c7a43b1a879af3e6ab880b332d539d80
Michael Roberts <michael@vivtek.com>
2021-03-12 21:53:27 -0400

    Updated to v0.03 and added a Change entry

--- lib/Data/Org/Template.pm
+++ lib/Data/Org/Template.pm
@@ -14,11 +14,11 @@ Data::Org::Template - template engine that plays well with iterators
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
