d5c4c1fa4ebf44da12dd24ceeb7c5e0f01b366d4
Michael Roberts <michael@vivtek.com>
2021-03-16 13:24:55 -0400

    Bumped version to 0.04 for CPAN

--- lib/Data/Org/Template.pm
+++ lib/Data/Org/Template.pm
@@ -14,11 +14,11 @@ Data::Org::Template - template engine that plays well with iterators
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
